f0dc9c27445665885834eeeec411ccff3c93b297
agarun <19801205+agarun@users.noreply.github.com>
2017-12-06 17:56:07 -0500

    Add middleware exceptions

--- lib/show_exceptions.rb
+++ lib/show_exceptions.rb
@@ -1,10 +1,21 @@
 require 'erb'
 
 class ShowExceptions
+  attr_reader :app
+  
   def initialize(app)
+    @app = app 
   end
 
   def call(env)
+    req = Rack::Request.new(env)
+    
+    begin
+      
+    rescue
+      
+    end
+    
   end
 
   private
