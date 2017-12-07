50e8e149a7e600f0afd6da2c80361f91ed21bd0e
agarun <19801205+agarun@users.noreply.github.com>
2017-12-06 23:41:22 -0500

    Add middleware exceptions view skeleton

--- lib/show_exceptions.rb
+++ lib/show_exceptions.rb
@@ -2,25 +2,36 @@ require 'erb'
 
 class ShowExceptions
   attr_reader :app
-  
+
   def initialize(app)
-    @app = app 
+    @app = app
   end
 
+  # the app *must* respond to `#call`
   def call(env)
-    req = Rack::Request.new(env)
-    
-    begin
-      
-    rescue
-      
-    end
-    
+    app.call(env)
+  rescue => error
+    render_exception(error)
   end
 
   private
 
-  def render_exception(e)
+  def render_exception(error)
+    html_error_template_path = File.dirname(__FILE__) + "/templates/rescue.html.erb"
+    html_error_template = File.read(html_error_template_path)
+    # use Kernel#binding to make instance methods available in the ERB view
+    rescue_body = ERB.new(html_error_template).result(binding)
+
+    [
+      '500',
+      {
+        'Content-type' => 'text/html'
+      },
+      rescue_body
+    ]
   end
 
+  def get_backtrace(error)
+    error.backtrace.slice(0, 2)
+  end
 end
