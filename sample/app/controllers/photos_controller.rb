4fa878a77ad0d8fd627b438b266d222613635d9f
agarun <19801205+agarun@users.noreply.github.com>
2018-01-16 17:51:25 -0500

    Add controller actions

--- sample/app/controllers/photos_controller.rb
+++ sample/app/controllers/photos_controller.rb
@@ -1,7 +1,19 @@
 require_relative "application_controller"
+require_relative "../models/photo"
+require_relative "../models/album"
 
 class PhotosController < ApplicationController
   def index
+    @photos = Photo.all
     render :index
+  end
+
+  def new
+    @photo = Photo.new(
+      photo_title: params[:photo][:photo_title],
+      photo_url: params[:photo][:photo_url],
+      user_id: rand(999),
+      album_id: rand(999),
+    )
   end
 end
