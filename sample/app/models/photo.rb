c323ca4c90d2a3cb55c800ba50dbd3bf524ee2ed
agarun <19801205+agarun@users.noreply.github.com>
2018-01-23 13:37:11 -0500

    Update photo model

--- sample/app/models/photo.rb
+++ sample/app/models/photo.rb
@@ -2,8 +2,6 @@ require_relative "application_record"
 
 class Photo < ApplicationRecord
   belongs_to :album
-  has_one_through :user,
-                  :album,
-                  :user
+  has_one_through :user, :album, :user
   finalize!
 end
