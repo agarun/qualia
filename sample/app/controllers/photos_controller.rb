d4557b80d3dc48db46bd7241c3c8b62f9a8b5bc7
agarun <19801205+agarun@users.noreply.github.com>
2018-01-31 14:20:44 -0500

    Add photos controller create and new actions

--- sample/app/controllers/photos_controller.rb
+++ sample/app/controllers/photos_controller.rb
@@ -1,5 +1,6 @@
 require_relative "application_controller"
 require_relative "../models/photo"
+require_relative "../models/user"
 require_relative "../models/album"
 
 class PhotosController < ApplicationController
@@ -9,11 +10,22 @@ class PhotosController < ApplicationController
   end
 
   def new
+    render :new
+  end
+
+  def create
     @photo = Photo.new(
-      photo_title: params[:photo][:photo_title],
-      photo_url: params[:photo][:photo_url],
-      user_id: rand(999),
-      album_id: rand(999),
+      photo_title: params['photos']['photo_title'],
+      photo_url: params['photos']['photo_url'],
+      user_id: User.all.sample.id,
+      album_id: Album.all.sample.id
     )
+
+    if @photo.save
+      @photos = Photo.all
+      render :index
+    else
+      render ['error: invalid fields'], status: 422
+    end
   end
 end
